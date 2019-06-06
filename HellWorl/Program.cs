81ae97b6881c26d08036f3a9bf756be06b1246a1
jingug1004 <jingug1004@gmail.com>
2019-06-06 12:34:17 +0900

    190606 (목) - 현충일 공휴일 대구 집에서

---
+++ HellWorl/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HellWorl
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
